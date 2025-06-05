5ddc8caa1f40ed58a0a6344fbf616a6a90a7613e
ignis <maildevcom+1@gmail.com>
2025-06-04 22:58:41 -0400

    fix: - packages;

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,25 +2,18 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2020,
-    sourceType: 'module',
-    project: './tsconfig.json'
+    sourceType: 'module'
   },
-  plugins: ['@typescript-eslint', 'prettier'],
+  plugins: ['@typescript-eslint'],
   extends: [
-    'eslint:recommended',
-    '@typescript-eslint/recommended',
-    '@typescript-eslint/recommended-requiring-type-checking',
-    'prettier'
+    'eslint:recommended'
   ],
   rules: {
-    'prettier/prettier': 'error',
+    'no-unused-vars': 'off', // Turn off base rule
     '@typescript-eslint/no-unused-vars': 'error',
-    '@typescript-eslint/explicit-function-return-type': 'warn',
-    '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/no-unsafe-assignment': 'warn',
-    '@typescript-eslint/no-unsafe-member-access': 'warn',
-    '@typescript-eslint/no-unsafe-call': 'warn',
-    '@typescript-eslint/no-unsafe-return': 'warn'
+    'no-console': 'off',
+    'prefer-const': 'error',
+    'no-var': 'error'
   },
   env: {
     node: true,
@@ -28,5 +21,5 @@ module.exports = {
     es6: true,
     jest: true
   },
-  ignorePatterns: ['dist/', 'node_modules/', '*.js']
+  ignorePatterns: ['dist/', 'node_modules/', '*.js', '**/*.d.ts']
 };
